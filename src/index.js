8904c56128430f36210c77e8eb3e9f6991aa377e
Eduardo <eduardoslzc@icloud.com>
2022-07-03 19:16:30 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+/**
+*Add two numbers
+* @param a Number one
+* @param b Number two
+* @returns Result
+*/
+export var add = function (a, b) { return a + b; };
+/**
+ * Substract two numbers
+ * @param a Number one
+ * @param b Number two
+ * @returns Result
+ */
+export var substract = function (a, b) { return a - b; };
+/**
+ * Multiplies two numbers
+ * @param a Number one
+ * @param b Number two
+ * @returns Result
+ */
+export var multiply = function (a, b) { return a * b; };
